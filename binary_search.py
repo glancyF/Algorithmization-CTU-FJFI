b5f31c419a099a1b20334ff4b32a527f3688a556
glancy <valentindesel@gmail.com>
2025-05-30 17:24:56 +0200

    Added binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,27 @@
+def binary_search(arr, low, high, x):
+    if high >= low:
+        mid = (high + low) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+        else:
+            return binary_search(arr, mid + 1, high, x)
+
+    else:
+        return -1
+
+
+def binary_search_it(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
